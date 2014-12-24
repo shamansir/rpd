ca81648d65be3528d8cc1284a695a30d36195b29
Ulric Wilfred <shaman.sir@gmail.com>
2014-12-24 23:14:06 +0100

    improving connection patterns

--- src/rpd.js
+++ src/rpd.js
@@ -14,25 +14,34 @@ function short_uid() {
     return ("0000" + (Math.random() * Math.pow(36,4) << 0).toString(36)).slice(-4);
 }
 
+function rev_cons(prev, cur) {
+    return [ cur, Array.isArray(prev) ? prev : [ prev, null ] ];
+};
+
+function walk_rev_cons(cell, f) {
+    if (!cell) return;
+    // rev_cons is not called for a stream with just one item, so cell
+    // may be a first object itself, unpacked. since we don't use arrays
+    // as values of these streams, it's a safe check
+    if (!Array.isArray(cell)) { f(cell); return; }
+    f(cell[0]); walk_cons(cell[1], f);
+}
+
 function Model() {
-    this.nodes = Kefir.emitter();
     this.targets = Kefir.emitter();
     this.renderers = Kefir.emitter();
 
-    function rev_cons(prev, cur) {
-        return [ cur, Array.isArray(prev) ? prev : [ prev, null ] ];
+    this.events = {
+        'node/add': Kefir.emitter(),
+        'node/remove': Kefir.emitter(),
     };
 
-    function walk_rev_cons(cell, f) {
-        if (!cell) return;
-        // rev_cons is not called for a stream with just one item, so cell
-        // may be a first object itself, unpacked. since we don't use arrays
-        // as values of these streams, it's a safe check
-        if (!Array.isArray(cell)) { f(cell); return; }
-        f(cell[0]); walk_cons(cell[1], f);
-    }
-
-    this.updates = Kefir.pool().plug(this.nodes);
+    this.updates = Kefir.pool().plug(this.events['node/add'].map(function(node) {
+                                         return { type: 'node/add', node: node };
+                                     }))
+                               .plug(this.events['node/remove'].map(function(node) {
+                                         return { type: 'node/remove', node: node };
+                                     }));
 
     Kefir.combine([ this.updates,
                     this.targets.scan(rev_cons),
@@ -55,19 +64,15 @@ Model.prototype.attachTo = function(elm) {
     return this;
 }
 Model.prototype.add = function(node) {
-    this.nodes.emit({
-        type: 'node/add',
-        subject: node
-    });
+    this.events['node/add'].emit(node);
     this.updates.plug(node.updates);
+    // TODO: node.turnOn
     return this;
 }
 Model.prototype.remove = function(node) {
-    this.nodes.emit({
-        type: 'node/remove',
-        subject: node
-    });
+    this.events['node/remove'].emit(node);
     this.updates.unplug(node.updates);
+    // TODO: node.turnOff
     return this;
 }
 Model.prototype.renderWith = function(alias) {
@@ -86,55 +91,46 @@ function Node(type, name) {
     this.name = name || def.name || 'Unnamed';
     this.def = def;
 
-    this.channels = Kefir.emitter();
-    this.links = Kefir.emitter();
-    this.updates = Kefir.pool().plug(this.channels).plug(this.links);
+    this.events = {
+        'inlet/add': Kefir.emitter(),
+        'inlet/remove': Kefir.emitter(),
+        'outlet/add': Kefir.emitter(),
+        'outlet/remove': Kefir.emitter()
+    };
+    this.updates = Kefir.pool().plug(this.events['inlet/add'].map(function(inlet) {
+                                         return { type: 'inlet/add', inlet: inlet };
+                                     }))
+                               .plug(this.events['inlet/remove'].map(function(inlet) {
+                                         return { type: 'inlet/remove', inlet: inlet };
+                                     }))
+                               .plug(this.events['outlet/add'].map(function(outlet) {
+                                         return { type: 'outlet/add', outlet: outlet };
+                                     }))
+                               .plug(this.events['outlet/remove'].map(function(outlet) {
+                                         return { type: 'outlet/remove', outlet: outlet };
+                                     }));
 }
-Node.prototype.addInlet = function(pos, type, name) {
-    var inlet = new Channel(type, 'in', this, pos, name);
-    this.channels.emit({
-        type: 'inlet/new',
-        subject: inlet
-    });
-    this.updates.plug(inlet.value.map(function(value) {
-        return {
-            type: 'inlet/update',
-            subject: [ inlet, value ]
-        }
-    }));
-    return this; // return inlet?
+Node.prototype.addInlet = function(type, name) {
+    var inlet = new Inlet(type, this, name);
+    this.events['inlet/add'].emit(inlet);
+    this.updates.plug(inlet.updates);
+    return inlet;
 }
-Node.prototype.addOutlet = function(pos, type, value, name) {
-    var outlet = new Channel(type, 'out', this, pos, name);
-    this.channels.emit({
-        type: 'outlet/new',
-        subject: outlet
-    });
-    this.updates.plug(outlet.value.map(function(value) {
-        return {
-            type: 'outlet/update',
-            subject: [ outlet, value ]
-        }
-    }));
-    outlet.set(value);
-    return this; // return outlet?
+Node.prototype.addOutlet = function(type, value, name) {
+    var outlet = new Outlet(type, this, name);
+    this.events['outlet/add'].emit(outlet);
+    this.updates.plug(outlet.updates);
+    outlet.send(value);
+    return outlet;
 }
-Node.prototype.removeChannel = function(channel) {
+/* Node.prototype.removeChannel = function(channel) {
     // TODO:
-}
-Node.prototype.connect = function(outlet_id, other, inlet_id, f) {
-    var link = new Link((f ? 'core/adapted' : 'core/direct'),
-                        outlet_id, this, inlet_id, f);
-    this.links.emit({
-        type: 'link/new',
-        subject: link
-    });
-    return this;
-}
+} */
 
-function Channel(type, dir, node, pos, name) {
+
+function Channel(what, type, node, name) {
     this.type = type || 'core/bool';
-    this.direction = dir;
+    this.what = what;
     var def = channeltypes[this.type];
     if (!def) report_error('Channel type ' + this.type + ' is not registered!');
     this.def = def;
@@ -142,13 +138,42 @@ function Channel(type, dir, node, pos, name) {
     this.name = name || def.name || 'Unnamed';
 
     this.value = Kefir.pool();
+
+    this.events = {};
+    this.events[what + '/update'] = Kefir.emitter().merge(this.value);
+    this.events[what + '/connect'] = Kefir.emitter();
+
+    this.updates = Kefir.pool().plug(this.events[what + '/update'].map(function(subj) {
+                                         var res = {};
+                                         res.type = what + '/update'; res[what] = subj;
+                                         return res;
+                                     }))
+                               .plug(this.events[what + '/connect'].map(function(subj) {
+                                         var res = {};
+                                         res.type = what + '/connect'; res[what] = subj;
+                                         return res;
+                                     }));
+
 }
-Channel.prototype.set = function(value) {
-    this.value.plug(value);
+Channel.prototype.connect = function(other, f) {
+    var link = new Link((f ? 'core/adapted' : 'core/direct'),
+                        this, other, f);
+    this.events[this.concavity + '/connect'].emit(link);
     return this;
 }
+Channel.prototype.send = function(value) {
+    this.value.plug(Kefir.constant(value));
+    return this;
+}
+Channel.prototype.stream = function(stream) {
+    this.value.plug(stream);
+    return this;
+}
+function Inlet() { return Channel.call(this, ['in'].concat(arguments)); }
+function Outlet() { return Channel.call(this, ['out'].concat(arguments)); }
+Outlet.prototype.send = Channel.prototype.send;
 
-function Link(type, from_pos, from_node, to_pos, to_node, f, name) {
+function Link(type, outlet, inlet, f, name) {
     this.type = type || 'core/direct';
     var def = linktypes[this.type];
     if (!def) report_error('Link type ' + this.type + ' is not registered!');
@@ -156,12 +181,12 @@ function Link(type, from_pos, from_node, to_pos, to_node, f, name) {
 
     this.name = name || def.name || '';
 
-    this.from_pos = from_pos;
-    this.from_node = from_node;
-    this.to_pos = to_pos;
-    this.to_node = to_node;
+    this.outlet = outlet;
+    this.inlet = inlet;
 
     this.adapter = f || def.f || undefined;
+
+    // TODO: link/convert event?
 }
 
 function nodetype(id, def) {
