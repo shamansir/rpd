f9e77f89c2ef8d40942fae9a2c69cc588ae4d460
Ulric Wilfred <shaman.sir@gmail.com>
2014-12-25 23:04:37 +0100

    CoreHtmlRenderer skeleton

--- src/render/core.html.js
+++ src/render/core.html.js
@@ -1,11 +1,24 @@
+var nodes = {};
+
+var CoreHtmlRenderer = {
+    'node/add': function(root, update) {
+
+    },
+    'node/remove': function(root, update) {},
+    'inlet/add': function(root, update) {},
+    'inlet/remove': function(root, update) {},
+    'inlet/update': function(root, update) {},
+    'outlet/add': function(root, update) {},
+    'outlet/remove': function(root, update) {},
+    'outlet/update': function(root, update) {},
+    'outlet/connect': function(root, update) {},
+    'link/adapt': function(root, update) {},
+    'link/error': function(root, update) {}
+};
+
 renderer('html', function(root, update) {
 
     console.log(root, update);
-
-    /* if (node.type == 'core/empty') {
-        var node_elm = document.createElement('span');
-        node_elm.innerText = node.name;
-        root.appendChild(node_elm);
-    } */
+    CoreHtmlRenderer[update.type](root, update);
 
 });
