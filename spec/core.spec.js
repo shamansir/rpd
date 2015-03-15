07ac854d5375c89b1be8efe786532529579081d1
Ulric Wilfred <shaman.sir@gmail.com>
2015-03-15 23:23:52 +0100

    first tests (of nothing)

---
+++ spec/core.spec.js
@@ -0,0 +1,39 @@
+describe("model", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+
+});
+
+describe("nodes", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(false);
+  });
+
+});
+
+describe("channels", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(false);
+  });
+
+});
+
+describe("links", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(false);
+  });
+
+});
+
+describe("renderers", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(false);
+  });
+
+});
