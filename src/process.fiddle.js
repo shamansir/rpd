df9a6dbeaf810ce78e3fdd17486e09c3ab53f82b
Ulric Wilfred <shaman.sir@gmail.com>
2015-06-09 18:54:10 +0200

    cleaner example

--- src/process.fiddle.js
+++ src/process.fiddle.js
@@ -2,42 +2,10 @@ Kefir.DEPRECATION_WARNINGS = false;
 
 console.log('------------------');
 
-var NO_VALUE = '__NONE__';
-function I(v) { return function() { return v }; }
-
 var inletAdd = Kefir.emitter(),
     outletAdd = Kefir.emitter();
 
 var process_f = function() { console.log('process', arguments[0], arguments[1]); return {}; };
-var inlets =
-    inletAdd.flatMap(function(inlet) {
-        var updates = inlet.update.map(function(value) {
-            return { inlet: inlet, alias: inlet.alias, value: value };
-        });
-        //if (myself.def.tune) updates = myself.def.tune(updates);
-        return updates;
-    });
-/*inlets = inlets.scan(function(values, update) {
-    var alias = update.alias,
-        inlet = update.inlet;
-    if (!values) {
-        var cur_values = {};
-        cur_values[alias] = update.value;
-        return { prev: {}, cur: cur_values, source: inlet };
-    } else {
-        if (values.cur[alias]) values.prev[alias] = values.cur[alias];
-        values.cur[alias]  = update.value;
-        values.source = inlet;
-        return values;
-    }
-}, null);*/
-/* inlets = inlets.filter(function(updates) {
-    return updates && !updates.source.cold;
-});
-var outlets = outletAdd.scan(function(outlets, outlet) {
-    outlets[outlet.alias] = outlet;
-    return outlets;
-}, {}); */
 var ready = Kefir.emitter();
 ready.onValue(function() { console.log('!! ready'); });
 var process = Kefir.combine([
@@ -64,8 +32,6 @@ process.bufferBy(ready).take(1).flatten()
            return data;
        }, { inlets: { prev: {}, cur: {} }, outlets: {} }).changes()
        .onValue(function(data) {
-            //var inlets_vals = value[0] || { prev: {}, cur: {} }; var outlets = value[1] || {};
-            //var outlets_vals = process_f(inlets_vals.cur, inlets_vals.prev);
             var outlets_vals = process_f(data.inlets.cur, data.inlets.prev);
             console.log('outlets', data.outlets, outlets_vals);
         });
