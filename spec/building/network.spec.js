65c2ddbca032e30c6a8e5776590f21a32c5ea849
Ulric Wilfred <shaman.sir@gmail.com>
2015-06-24 15:57:10 +0200

    first network specs

--- spec/building/network.spec.js
+++ spec/building/network.spec.js
@@ -1,7 +1,29 @@
-describe('network', function() {
+xdescribe('building: network', function() {
 
-    it('should work', function() {
-        expect('works').toBe('works');
+    it('should always be there and should be a single one', function() {
+        expect(Rpd.network).toBeDefined();
+    });
+
+    it('should fire an event when new model was started', function() {
+        var modelStartSpy;
+
+        Rpd.network.event['model/start'].onValue(modelStartSpy);
+
+        var model = Rpd.Model.start();
+
+        expect(modelStartSpy).toHaveBeenCalledWith(model);
+    });
+
+    it('should allow several models to start', function() {
+        var modelStartSpy;
+
+        Rpd.network.event['model/start'].onValue(modelStartSpy);
+
+        var model1 = Rpd.Model.start();
+        var model2 = Rpd.Model.start();
+
+        expect(modelStartSpy).toHaveBeenCalledWith(model1);
+        expect(modelStartSpy).toHaveBeenCalledWith(model2);
     });
 
 });
--- spec/history/history.spec.js
+++ spec/history/history.spec.js
@@ -1,7 +1,20 @@
 describe('history', function() {
 
+    // node/add
+    // node/turn-off
+    // outlet/connect
+    // link/disable
+    // inlet/update
+    // outlet/update
+    //
+    // move node?
+
+    // other:
+
     it('should work', function() {
         expect('works').toBe('works');
     });
+
+    //node.extractToType()
 
 });
