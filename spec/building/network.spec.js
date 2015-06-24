6ac3a251789f671745cf77209dcca4963cc33745
Ulric Wilfred <shaman.sir@gmail.com>
2015-06-24 17:22:01 +0200

    more network specs drafts

--- spec/building/network.spec.js
+++ spec/building/network.spec.js
@@ -26,4 +26,20 @@ xdescribe('building: network', function() {
         expect(modelStartSpy).toHaveBeenCalledWith(model2);
     });
 
+    it('should not pass updates to renderer if it was not specified', function() {
+
+    });
+
+    it('should pass updates to all specified renderers and models if their targets are different', function() {
+
+    });
+
+    it('should allow and track switching current model', function() {});
+
+    it('after the switch, starts passing updates to the same renderer with same target', function() {});
+
+    it('fires all construction updates of model selected as current, to a renderer, after the switch', function() {});
+
+    it('fires construction updates again and again if models were switched back', function() {});
+
 });
