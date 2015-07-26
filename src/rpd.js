ac192fa76b797013fadbdf239b282d33406b8751
Ulric Wilfred <shaman.sir@gmail.com>
2015-07-27 00:45:16 +0200

    actual: allow not to return handler object of function for renderer, and fix tests with that

--- src/rpd.js
+++ src/rpd.js
@@ -81,14 +81,14 @@ function Patch(name) {
                         }
                         if (renderer.produce) {
                             var handler = renderer.produce(target, configuration);
-                            //if (handler) {
+                            if (handler) {
                                 renderer.handlers.push(
                                     (typeof handler === 'function') ? handler
                                                                     : function(event) {
                                                                         if (handler[event.type]) handler[event.type](event);
                                                                       }
                                 );
-                            //}
+                            }
                         }
                         return renderers;
                     }, { }) ]);
