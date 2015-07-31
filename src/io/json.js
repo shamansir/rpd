576fc481e697d9b373c155b03f3f9fb6573f8e13
Ulric Wilfred <shaman.sir@gmail.com>
2015-07-31 23:13:03 +0200

    draft for import/export, p.2

--- src/io/json.js
+++ src/io/json.js
@@ -3,5 +3,23 @@ Rpd.import.json = function() {
 }
 
 Rpd.export.json = function() {
+    var json = {
+        nodes: {}
+    };
 
+    var spec = {
+        'patch/add-node': function(update) {
+            var node = update.node;
+            json.nodes[node.id] = {
+                type: node.type,
+                name: node.name
+            }
+        }
+    };
+
+    Rpd.events.onValue(function(update) {
+        if (spec[update.type]) spec[update.type](update);
+    });
+
+    return json;
 }
