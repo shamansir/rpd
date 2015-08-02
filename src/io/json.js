11855bfe9d7e56bfe581e908fa61adcb9b3db648
Ulric Wilfred <shaman.sir@gmail.com>
2015-08-02 11:19:45 +0200

    filling up JSON import

--- src/io/json.js
+++ src/io/json.js
@@ -1,23 +1,6 @@
 ;(function(global) {
   "use strict";
 
-Rpd.import.json = function(json) {
-
-    var spec = makeImportSpec();
-
-    if ((json.version !== Rpd.VERSION) && console && console.warn) {
-        console.warn('JSON version', json.version, 'and RPD Version',
-                                     Rpd.VERSION, 'are not equal to each other');
-    }
-
-    var commands = json.commands;
-
-    for (var i = 0; i < commands.length; i++) {
-        if (spec[commands[i].command]) spec[commands[i].command](commands[i]);
-    }
-
-}
-
 Rpd.export.json = function(name) {
     var spec = exportSpec;
 
@@ -34,6 +17,23 @@ Rpd.export.json = function(name) {
 
     return json;
 }
+
+Rpd.import.json = function(json) {
+    var spec = makeImportSpec();
+
+    if ((json.version !== Rpd.VERSION) && console && console.warn) {
+        console.warn('JSON version', json.version, 'and RPD Version',
+                                     Rpd.VERSION, 'are not equal to each other');
+    }
+
+    var commands = json.commands;
+
+    for (var i = 0; i < commands.length; i++) {
+        if (spec[commands[i].command]) spec[commands[i].command](commands[i]);
+    }
+}
+
+// ================================= EXPORT =================================
 
 var exportSpec = {
     'network/add-patch': function(update) {
@@ -65,10 +65,10 @@ var exportSpec = {
     },
     'patch/add-node': function(update) {
         var node = update.node;
-        return { command: 'patch/add-node', nodeType: node.type, nodeName: node.name, nodeId: node.id };
+        return { command: 'patch/add-node', patchId: node.patch.id, nodeType: node.type, nodeName: node.name, nodeId: node.id };
     },
     'patch/remove-node': function(update) {
-        return { command: 'patch/remove-node', nodeId: update.node.id };
+        return { command: 'patch/remove-node', patchId: update.patch.id, nodeId: update.node.id };
     },
     'node/turn-on': function(update) {
         return { command: 'node/turn-on', nodeId: update.node.id };
@@ -98,7 +98,7 @@ var exportSpec = {
                  linkType: link.type, linkId: link.id }; /* TODO: adapter? */
     },
     'outlet/disconnect': function(update) {
-        return { command: 'outlet/disconnect', linkId: update.link.id };
+        return { command: 'outlet/disconnect', outletId: update.outlet.id, linkId: update.link.id };
     },
     'link/enable': function(update) {
         return { command: 'link/enable', linkId: update.link.id };
@@ -107,6 +107,8 @@ var exportSpec = {
         return { command: 'link/disable', linkId: update.link.id };
     }
 };
+
+// ================================= IMPORT =================================
 
 function makeImportSpec() {
     var patches = {},
@@ -125,21 +127,59 @@ function makeImportSpec() {
         'patch/exit': function(command) {
             patches[command.patchId].exit();
         },
-        'patch/set-inputs': function(command) {},
-        'patch/set-outputs': function(command) {},
+        'patch/set-inputs': function(command) {
+            var inputs = command.inputs,
+                inputsTrg = [];
+            for (var i = 0; i < inputs.length; i++) {
+                inputsTrg.push(inlets[inputs[i]]);
+            }
+            patches[command.patchId].inputs(inputsTrg);
+        },
+        'patch/set-outputs': function(command) {
+            var outputs = command.outputs,
+                outputsTrg = [];
+            for (var i = 0; i < outputs.length; i++) {
+                outputsTrg.push(inlets[outputs[i]]);
+            }
+            patches[command.patchId].outputs(outputsTrg);
+        },
         'patch/refer': function(command) {},
-        'patch/add-node': function(command) {},
-        'patch/remove-node': function(command) {},
-        'node/turn-on': function(command) {},
-        'node/turn-off': function(command) {},
-        'node/add-inlet': function(command) {},
-        'node/remove-inlet': function(command) {},
-        'node/add-outlet': function(command) {},
-        'node/remove-outlet': function(command) {},
-        'outlet/connect': function(command) {},
-        'outlet/disconnect': function(command) {},
-        'link/enable': function(command) {},
-        'link/disable': function(command) {}
+        'patch/add-node': function(command) {
+            nodes[command.nodeId] = patches[command.patchId].addNode(command.nodeType, command.nodeName);
+        },
+        'patch/remove-node': function(command) {
+            patches[command.patchId].removeNode(nodes[command.nodeId]);
+        },
+        'node/turn-on': function(command) {
+            nodes[command.nodeId].turnOn();
+        },
+        'node/turn-off': function(command) {
+            nodes[command.nodeId].turnOff();
+        },
+        'node/add-inlet': function(command) {
+            inlets[command.Id] = nodes[command.nodeId].addInlet(command.inletType, command.inletName, command.inletAlias);
+        },
+        'node/remove-inlet': function(command) {
+            nodes[command.nodeId].removeInlet(nodes[command.inletId]);
+        },
+        'node/add-outlet': function(command) {
+            outlets[command.Id] = nodes[command.nodeId].addOutlet(command.outletType, command.outletName, command.outletAlias);
+        },
+        'node/remove-outlet': function(command) {
+            nodes[command.nodeId].removeOutlet(nodes[command.outletId]);
+        },
+        'outlet/connect': function(command) {
+            links[command.linkId] = outlets[command.outletId].connect(inlets[command.inletId], null, command.linkType);
+        },
+        'outlet/disconnect': function(command) {
+            outlets[command.outletId].disconnect(links[command.linkId]);
+        },
+        'link/enable': function(command) {
+            links[command.linkId].enable();
+        },
+        'link/disable': function(command) {
+            links[command.linkId].disable();
+        }
     }
 }
 
