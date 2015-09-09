4414c6b3af5cf8796a62a90a546438ba87eab6b8
Ulric Wilfred <shaman.sir@gmail.com>
2015-09-09 20:46:35 +0200

    channel editor style

--- src/toolkit/core/render/svg.js
+++ src/toolkit/core/render/svg.js
@@ -18,7 +18,7 @@ Rpd.channelrenderer('core/number', 'svg', {
     /* show: function(target, value) { }, */
     edit: function(target, inlet, valueIn) {
         var foElm = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
-        foElm.setAttributeNS(null, 'width', 30);
+        foElm.setAttributeNS(null, 'width', 20);
         foElm.setAttributeNS(null, 'height', 30);
         var valInput = document.createElementNS('http://www.w3.org/1999/xhtml', 'input');
         valInput.type = 'number';
