3781501d95b763dfb7b24d3e214a1a493a55c3bb
Ulric Wilfred <shaman.sir@gmail.com>
2015-09-29 18:59:35 +0200

    PD model draft

---
+++ src/toolkit/pd/model.js
@@ -0,0 +1,14 @@
+var PdModel = (function() {
+
+    /* function Message(selector, atoms) {
+
+    } */
+
+    return {
+        //Message: Message,
+        objects: {},
+        editMode: true,
+        selection: null
+    };
+
+});
--- src/toolkit/pd/toolkit.js
+++ src/toolkit/pd/toolkit.js
@@ -1,32 +1,32 @@
 Rpd.linktype('core/pass', { });
 
-Rpd.channeltype('pd/atom', { });
+Rpd.channeltype('pd/msg', { });
 
 Rpd.nodetype('pd/object', { });
 
 Rpd.nodetype('pd/text', {
-    inlets: { 'text': { type: 'pd/atom', hidden: true } }
+    inlets: { 'text': { type: 'pd/msg', hidden: true } }
 });
 
 Rpd.nodetype('pd/gatom', {
-    inlets: { '0': { type: 'pd/atom' } },
-    outlets: { '0': { type: 'pd/atom' } }
+    inlets: { '0': { type: 'pd/msg' } },
+    outlets: { '0': { type: 'pd/msg' } }
 }); // a.k.a. symbol
 
-Rpd.nodetype('pd/message', {
-    inlets: { '0': { type: 'pd/atom' },
-              'v': { type: 'pd/atom', hidden: true } },
-    outlets: { '0': { type: 'pd/atom' } }
+Rpd.nodetype('pd/msg', {
+    inlets: { '0': { type: 'pd/msg' },
+              'v': { type: 'pd/msg', hidden: true } },
+    outlets: { '0': { type: 'pd/msg' } }
 });
 
 Rpd.nodetype('pd/bang', {
-    inlets: { 'in': { type: 'pd/atom' },
-              'v': { type: 'pd/atom', hidden: true } },
-    outlets: { 'out': { type: 'pd/atom' } }
+    inlets: { 'in': { type: 'pd/msg' },
+              'v': { type: 'pd/msg', hidden: true } },
+    outlets: { 'out': { type: 'pd/msg' } }
 });
 
 Rpd.nodetype('pd/toggle', {
-    inlets: { 'in': { type: 'pd/atom' },
-              'v': { type: 'pd/atom', hidden: true } },
-    outlets: { 'out': { type: 'pd/atom' } }
+    inlets: { 'in': { type: 'pd/msg' },
+              'v': { type: 'pd/msg', hidden: true } },
+    outlets: { 'out': { type: 'pd/msg' } }
 });
