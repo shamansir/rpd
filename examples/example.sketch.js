8f28c0356c8f8feb51746ba4634b842c65aa1322
Ulric Wilfred <shaman.sir@gmail.com>
2015-11-27 18:58:42 +0100

    add shape selection to p5.js example

--- examples/example.sketch.js
+++ examples/example.sketch.js
@@ -4,15 +4,25 @@ var sketchConfig = {
   amplitude: 75.0,    // Height of wave
   period: 500.0,      // How many pixels before the wave repeats
   startcolor: { r: 255, g: 255, b: 255 }, // Color of the first wave
-  endcolor: { r: 255, g: 255, b: 255 }    // Color of the last wave
+  endcolor: { r: 255, g: 255, b: 255 },   // Color of the last wave
+  shape: 'circle'
 };
+
+var SHAPE_FUNC = { circle: function(x, y) { ellipse(x, y, 14, 14); },
+                   rect: function(x, y) { rect(x, y, 14, 14); },
+                   cross: function(x, y) { line(x-7, y-7, x+7, y+7);
+                                           line(x+7, y-7, x-7, y+7); },
+                   diamond: function(x, y) {
+                      quad(x, y-7, x+7, y, x, y+7, x-7, y);
+                   } };
 
 var w,  // Width of entire wave
     dx, // Value for incrementing x
     theta = 0.0, // Start angle at 0
     yvalues,  // Using an array to store height values for the wave
     start, // start color in p5 format
-    end;  // end color in p5 format
+    end,
+    shapefunc;  // end color in p5 format
 
 function setup() {
   var myCanvas = createCanvas(400, 300);
@@ -36,6 +46,7 @@ function updateWithConfig(conf) {
                           : color(255, 255, 255);
   end = conf.endcolor ? color(conf.endcolor.r, conf.endcolor.g, conf.endcolor.b)
                       : color(255, 255, 255);
+  shapefunc = SHAPE_FUNC[conf.shape || 'circle'];
 }
 
 function calcWaves(conf) {
@@ -57,8 +68,9 @@ function renderWaves(conf) {
   var colorspan = 1 / conf.wavescount;
   for (var i = 0, y = 20; i < conf.wavescount; i++, y += yspan) {
     fill(lerpColor(start, end, colorspan * i));
+    stroke(lerpColor(start, end, colorspan * i));
     for (var x = 0; x < yvalues.length; x++) {
-      ellipse(x*conf.xspacing, y+yvalues[x], 14, 14);
+      shapefunc(x*conf.xspacing, y+yvalues[x]);
     }
   }
 }
