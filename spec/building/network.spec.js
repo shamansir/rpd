1ebb1bee4b7eb88ba5c2a4038383629344872a0a
Ulric Wilfred <shaman.sir@gmail.com>
2016-03-04 23:20:27 +0100

    test inputs/outputs and projecting an inner patch

--- spec/building/network.spec.js
+++ spec/building/network.spec.js
@@ -17,12 +17,55 @@ describe('building: network', function() {
         it('inlet could be defined as patch output', function() {
             withNewPatch(function(patch, updateSpy) {
                 var node = patch.addNode('spec/empty');
-                var inlet = node.addOutlet('spec/any', 'foo');
+                var inlet = node.addInlet('spec/any', 'foo');
                 patch.inputs([ inlet ]);
 
                 expect(updateSpy).toHaveBeenCalledWith(
                     jasmine.objectContaining({ type: 'patch/set-inputs',
                                                inputs: jasmine.arrayContaining([ inlet ]) }));
+            });
+        });
+
+        it('both outlets and inlets could be defined together', function() {
+            withNewPatch(function(patch, updateSpy) {
+                var node = patch.addNode('spec/empty');
+                var inletOne  = node.addInlet('spec/any', 'foo');
+                var inletTwo  = node.addInlet('spec/any', 'foo');
+                var outletOne = node.addOutlet('spec/any', 'foo');
+                var outletTwo = node.addOutlet('spec/any', 'foo');
+                patch.inputs([ inletOne, inletTwo ]);
+                patch.outputs([ outletOne, outletTwo ]);
+
+                expect(updateSpy).toHaveBeenCalledWith(
+                    jasmine.objectContaining({ type: 'patch/set-inputs',
+                                               inputs: jasmine.arrayContaining([ inletOne, inletTwo ]) }));
+                expect(updateSpy).toHaveBeenCalledWith(
+                    jasmine.objectContaining({ type: 'patch/set-outputs',
+                                               outputs: jasmine.arrayContaining([ outletOne, outletTwo ]) }));
+            });
+        });
+
+        it('inner patch could be projected', function() {
+            withNewPatch(function(rootPatch, updateSpy) {
+                var rootNode = rootPatch.addNode('spec/empty');
+
+                var innerPatch = Rpd.addPatch();
+                var innerNodeOne = innerPatch.addNode('spec/empty');
+                var inputOne = innerNodeOne.addInlet('spec/any', 'foo');
+                var outputOne = innerNodeOne.addOutlet('spec/any', 'foo');
+                var outputTwo = innerNodeOne.addOutlet('spec/any', 'foo');
+                var innerNodeTwo = innerPatch.addNode('spec/empty');
+                var outputThree = innerNodeTwo.addOutlet('spec/any', 'foo');
+
+                innerPatch.inputs([ inputOne ]);
+                innerPatch.outputs([ outputOne, outputTwo, outputThree ]);
+
+                innerPatch.project(rootNode);
+
+                expect(updateSpy).toHaveBeenCalledWith(
+                    jasmine.objectContaining({ type: 'patch/refer',
+                                               node: rootNode,
+                                               target: innerPatch }));
             });
         });
 
