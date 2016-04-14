c583cd07ccab733e1dac6238841fcfe1ee7550ed
Ulric Wilfred <shaman.sir@gmail.com>
2016-04-14 18:14:42 +0200

    docs: trying to add logo patch

--- docs/index-patches.js
+++ docs/index-patches.js
@@ -1,4 +1,11 @@
 function applyRpdLogoPatch(logoElm, patchElm) {
+
+    Rpd.renderNext('svg', patchElm, { style: 'quartz' });
+
+    Rpd.addPatch('test');
+
+    Rpd.addNode('core/empty', 'Foo');
+
     //alert('Rpd: ' + Rpd);
     //alert('applyRpdLogoPatch ' + logoElm + ' ' + patchElm);
 }
