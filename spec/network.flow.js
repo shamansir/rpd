e5ec92fb0fd6064466e57fb762e678f0f24a4dc1
Ulric Wilfred <shaman.sir@gmail.com>
2017-01-07 21:59:43 +0100

    add `flow check` to `gulp test` command

--- spec/network.flow.js
+++ spec/network.flow.js
@@ -1,7 +1,7 @@
 /* @flow */
 
-import Rpd from '../rpd';
+import Rpd from '../src/rpd.js';
 
-import type {Patch} from '../rpd';
+import type {Patch} from '../rpd.js.flow';
 
 const patch: Patch = Rpd.addPatch('foo');
