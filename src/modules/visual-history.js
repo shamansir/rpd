54a6055c89644f7b097225ce0c1cd3690690fefc
Ulric Wilfred <shaman.sir@gmail.com>
2017-01-08 19:12:18 +0100

    basic structure to render updates

--- src/modules/visual-history.js
+++ src/modules/visual-history.js
@@ -6,9 +6,35 @@ if (typeof Rpd === "undefined" && typeof require !== "undefined") {
     Rpd = require('rpd');
 }
 
+var d3 = d3_tiny || d3;
+
+var renderUpdate = {
+
+}
+
+var UPDATES_LIMIT = 500;
+
 Rpd.visualHistory = function(target, type) {
-    Rpd.events.onValue(function(v) {
-        console.log(v);
+    var ul = d3.select(target).classed('visual-history', true)
+               .append('ul');
+    var li, div, updateType;
+    var updatesCount = 0;
+    var updatesInList = 0;
+    Rpd.events.onValue(function(update) {
+        updateType = update.type;
+        if (updatesInList > UPDATES_LIMIT) {
+            ul.selectAll('*').remove();
+            updatesInList = 0;
+        }
+        li = ul.append('li').attr('id', 'update-' + updatesCount);
+        li.append('span').classed('update-type', true).text(updateType);
+        div = li.append('div').classed('update-details', true);
+        if (renderUpdate[updateType]) {
+            div.append(renderUpdate[updateType](update));
+        } else {
+            div.append('span').text('<Unknown>');
+        }
+        updatesCount++; updatesInList++;
     });
 }
 
